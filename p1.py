bc434bbb38925d2f7d1f5411b2a69fb0776a41c2
gerbil742 <jycampbell@mail.usf.edu>
2018-09-17 01:38:51 -0400

    committed my p1.py

--- p1.py
+++ p1.py
@@ -1,10 +1,20 @@
+"""
+Jibreel Campbell
+Abdullah Alsayyar
+Andres Delgado
+
+Project 1: DP-based SAT solver
+
+9/20/18
+"""
+
 def proveFormula(F):
 	###########################################################
-	# note: All these if statements are just used for testing # 
+	# note: All these if statements are just used for testing #
 	###########################################################
 
 	print("Given:     ", F)
-		
+
 	#Stores a binary tree list of the given input F
 	formulaList = MakeList(F)
 	if formulaList:
@@ -23,13 +33,15 @@ def proveFormula(F):
 	if cnfList:
 		print("1.	Elim Ops:     ", cnfList)
 	else: return
-	
-	# Steps 2/3 combined 
+
+	# Steps 2/3 combined
 	cnfList = DeMorgans(cnfList)
 	if cnfList:
 		print("2/3.	DM/Negations: ", cnfList)
 	else: return
 
+
+	#
 	#print(formulaList)
 	return "Good"
 
@@ -53,7 +65,7 @@ def EliminateOps(formula):
 		if type(arg) is list:
 			nestedArgs[i] = True
 		i += 1
-	
+
 	# IF operation: [IF A B] -> [OR [NOT A] B]
 	if (opIndex == 2) and (len(formula) == 3):
 		cnf.append('OR')
@@ -73,14 +85,14 @@ def EliminateOps(formula):
 			cnf.append(['NOT', formula[1]])
 			cnf.append(formula[2])
 			return cnf
-		
+
 	# Checks the formula for nested arguments and searches inside them
 	# Appends each argument to cnf
 	cnf.append(formula[0])
 	for arg in range(1, len(formula)):
 		if type(formula[arg]) is list:
 			cnf.append(EliminateOps(formula[arg]))
-		else: 
+		else:
 			cnf.append(formula[arg])
 	return cnf
 
@@ -101,11 +113,11 @@ def DeMorgans(formula, negation = False):
 			# Applies De Morgan's laws to the nested argument
 			return DeMorgans(formula[1], True)
 
-		# Case where there is a double negation on a signle variable	
+		# Case where there is a double negation on a signle variable
 		elif negation:
 			return formula[1]
 		return formula
-	
+
 	# Operator is AND/OR if this line is reached
 	# Flips AND/OR if applying De Morgan's
 	if negation:
@@ -120,7 +132,7 @@ def DeMorgans(formula, negation = False):
 			# Applies De Morgan's to nested argument
 			if negation:
 				formula[arg] = DeMorgans(formula[arg], True)
-			else: formula[arg] = DeMorgans(formula[arg])	
+			else: formula[arg] = DeMorgans(formula[arg])
 		# Negates the single variable
 		elif negation:
 			formula[arg] = ['NOT', formula[arg]]
@@ -128,7 +140,7 @@ def DeMorgans(formula, negation = False):
 
 def MakeList(F):
 	"""Takes original F input and returns a binary tree list, or a string if it's a single variable.
-	If F has more than 1 expression, 
+	If F has more than 1 expression,
 		Index 0 is the operator and index 1/2 are expressions (another binary tree list or variable).
 	else, returns the variable string.
 	"""
@@ -203,30 +215,89 @@ def FindVariables(formula, variables = None):
 
 	# formula is a single variable
 	elif formula[0] not in variables:
-		variables.append(formula[0])	
+		variables.append(formula[0])
 	return variables
 
-if __name__ == "__main__":
-	
-	problems = [#'p',
-	#'(NOT (NOT (NOT (NOT not))  )		)',
-	#'(IF p p)',
-	#'(AND p (NOT p) q (NOT t) gf)',
-	#'(OR five (OR three four))',
-	'(IF (IF (NOT p) (NOT q)) (IF p q))', 	### Example from slide 59
-	'(IF (AND q123 (NOT p) t) p)', 			#Test DM
-	#'(IF (NOT (OR p q123)) (AND q123 (NOT p)))',
-	#'anAtom123',
-	'(NOT (NOT (NOT (IF p q))))',			#Test DM 
-	'(NOT (AND p q))',            			#Test DM
-	#'(NOT (NOT (AND p q)))'       			#Test DM
-	]
-	
-	'''
-	problems = ['(IF a b)',
-	'(AND (NOT a) b c d)']
-	'''
+def resolution(clausalFormFormula, vars):
+	"""
+	performs the resolution rule on a bool formula in clausal form:
 
-	for i in range(len(problems)):
-		#print("Answer: ", end = '')
-		print(proveFormula(problems[i]), end = '\n\n')
+	Given some CNF formula F
+	For some variable p:
+		For every clause C1 containing p:
+			For every clause C2 containing ¬p:
+				Create the resolvent CR from C1 and C2, add it to assignments
+				NOTE: even if CR is empty, it must be kept!
+	Remove all clauses containing p or ¬p from F
+
+	clausalForm = a list of clauses representing a cnf formula
+	[ [p, q], [[p]], [p, [q], r, s, t, u, v] ]
+	if only considering the 'q's,the function will return this:
+	[ [[p]], [p, p, r, s, t, u, v] ]
+	the second term is the resolvent of the 1st and 3rd terms of the previous
+
+	vars = a list containing all the variables found in the statement
+	[p, q, r, s, t, u, v]
+
+
+	a list within an inner list if considered a negative variable
+	"""
+
+	 # Will store all of the statements after resolution operation has finished
+	expressionWithResolution = [];
+
+	for var in vars: # apply resolution to each variable
+		clausesWithVar = findClausesWithVariable(clausalFormFormula, var) # a list containing the clauses that use the var
+		clausesWithNegVar = findClausesWithVariable(clausalFormFormula, [var])
+
+		#print('\nvar:', var, '\nclauses:\n', clausesWithVariable)
+		print('\nvar:', var, '\nclauses:\n', clausesWithVar)
+		print('\nvar:', var, '\nneg clauses:\n', clausesWithNegVar)
+
+
+	return expressionWithResolution;
+
+def findClausesWithVariable(clausalFormFormula, variable):
+	"""
+	takes a clausal form boolean formula and returns a list storing the clauses
+	that contain the variable
+
+	to find the negative version of the variable, pass in a list containing only
+	the variable
+	"""
+	clausesWithVar = []
+
+
+	for clause in clausalFormFormula:
+		if variable in clause: # variabies
+			clausesWithVar.append(clause)
+
+	return clausesWithVar
+
+if __name__ == "__main__":
+
+	# problems = [#'p',
+	# #'(NOT (NOT (NOT (NOT not))  )		)',
+	# #'(IF p p)',
+	# #'(AND p (NOT p) q (NOT t) gf)',
+	# #'(OR five (OR three four))',
+	# '(IF (IF (NOT p) (NOT q)) (IF p q))', 	### Example from slide 59
+	# '(IF (AND q123 (NOT p) t) p)', 			#Test DM
+	# #'(IF (NOT (OR p q123)) (AND q123 (NOT p)))',
+	# #'anAtom123',
+	# '(NOT (NOT (NOT (IF p q))))',			#Test DM
+	# '(NOT (AND p q))',            			#Test DM
+	# #'(NOT (NOT (AND p q)))'       			#Test DM
+	# ]
+	#
+	# '''
+	# problems = ['(IF a b)',
+	# '(AND (NOT a) b c d)']
+	# '''
+
+	# for i in range(len(problems)):
+	# 	#print("Answer: ", end = '')
+	# 	print(proveFormula(problems[i]), end = '\n\n')
+
+	test = [ ['p', 'q'], [['p'], 'u'], ['p', ['q'], 'r', 's', 't', 'u', 'v'] ]
+	print("\nfinal result", resolution(test, ['p', 'q', 'r', 's', 't', 'u', 'v']))
